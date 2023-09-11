2e7655c94b8f9d5e86f3ee6b16e7fdca904afec0
kota-done <shinigami0706@gmail.com>
2023-09-11 11:39:52 +0900

    操作に伴う完了やエラーメッセージの表示とアイコン表示の追加

--- src/main/java/com/example/todolist/controller/TodoListController.java
+++ src/main/java/com/example/todolist/controller/TodoListController.java
@@ -2,6 +2,7 @@ package com.example.todolist.controller;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.todolist.common.OpMsg;
 import com.example.todolist.dao.TodoDaoImpl;
 import com.example.todolist.entity.Todo;
 import com.example.todolist.form.TodoData;
@@ -37,6 +40,7 @@ public class TodoListController {
 	private final HttpSession session;
 	private final TodoRepository todoRepository;
 	private final TodoService todoService; //データベース操作用で追加
+	private final MessageSource messageSource;
 
 	@GetMapping("/todo")
 	public ModelAndView showTodoList(ModelAndView mv,
@@ -66,20 +70,27 @@ public class TodoListController {
 
 	@PostMapping("/todo/create/do")
 	public String createTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result, //TodoDataオブジェクトにバインドし、この時点でidはnull
-			Model model, Locale locale) {
+			Model model, RedirectAttributes redirectAttributes, Locale locale) {
 		boolean isValid = todoService.isValid(todoData, result, true, locale);
 		//serviceクラスのバリデーションもクリアしたら登録処理に移る
 		if (!result.hasErrors() && isValid) {
 			Todo todo = todoData.toEntity();
 			//saveAndFlushのメソッド処理で「カラム毎の内容をデータベースに登録」までをスプリングで実行してくれる。レポジトリクラスで実装しているから可能。
 			todoRepository.saveAndFlush(todo);
+			String msg = messageSource.getMessage("msg.i.todo_created", null, locale);
+			//９/11 リダイレクトの場合、Model経由はデータ保持のスコープ外。redirectAttributeを使用する
+			redirectAttributes.addFlashAttribute("msg", new OpMsg("I", msg));
 			return "redirect:/todo/query";
-			//			return showTodoList(mv);　アドレスがtodo/createのままで一覧表示のメソッドを動かそうとすると、再読み込みが発生し、
+			//return showTodoList(mv);　アドレスがtodo/createのままで一覧表示のメソッドを動かそうとすると、再読み込みが発生し、
 			//実行するとブラウザは直前動作も一緒に実行するため「登録・画面表示」の二つが実行されてしまう。そのためリダイレクトで直接URLを移動してからメソッドを実行するように設定する。
 		} else {
 			//			mv.setViewName("todoForm");
 			//＠ModelAttributeがあることでmv.addObject("todoData",todoData)の記述が不要となる。自動的に発生したデータを遷移先でも表示可能。しかし、第一引数は使用するモデルクラス（頭文字は小文字である必要がある。
 			//エラー発生時はModelAndViewの機能は不要のため、ビューに渡すデータの保持（Model）を使用するだけで良い。
+
+			//9/11追加　操作メッセージ用
+			String msg = messageSource.getMessage("msg.e.input_something_wrong", null, locale);
+			model.addAttribute("msg", new OpMsg("E", msg)); //"msg"という名称のOpMsgオブジェクトを生成、メッセージ種別はErrorのため"E"を引数にしている。
 			return "todoForm";
 		}
 	}
@@ -103,24 +114,33 @@ public class TodoListController {
 	//更新
 	@PostMapping("/todo/update")
 	public String updateTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result, Model model,
-			Locale locale) {
+			RedirectAttributes redirectAttributes, Locale locale) {
 		//エラーチェック
 		boolean isValid = todoService.isValid(todoData, result, false, locale);
 		if (!result.hasErrors() && isValid) {
 			//エラーがない場合
 			Todo todo = todoData.toEntity(); //エンティティオブジェクト
 			todoRepository.saveAndFlush(todo);
+			String msg = messageSource.getMessage("msg.i.todo_updated", null, locale);
+			//９/11 リダイレクトの場合、Model経由はデータ保持のスコープ外。redirectAttributeを使用する
+			//	model.addAttribute("msg", new OpMsg("I", msg));
+			redirectAttributes.addFlashAttribute("msg", new OpMsg("I", msg));
 			return "redirect:/todo/query"; //リスト一覧に戻す
 		} else {
-			//エラーがあった場合、
+			//エラーがあった場合、メッセージ添付
+			String msg = messageSource.getMessage("msg.e.input_something_wrong", null, locale);
+			model.addAttribute("msg", new OpMsg("E", msg));
 			return "todoForm";
 		}
 	}
 
 	//削除
 	@PostMapping("/todo/delete")
-	public String deleteTodo(@ModelAttribute TodoData todoData) {
+	public String deleteTodo(@ModelAttribute TodoData todoData, RedirectAttributes redirectAttributes, Locale locale) {
 		//更新画面の表示内容からIDを取得して、それに該当するデータ内容を削除する。
+		String msg = messageSource.getMessage("msg.i.todo_deleted", null, locale);
+		//９/11 リダイレクトの場合、Model経由はデータ保持のスコープ外。redirectAttributeを使用する
+		redirectAttributes.addFlashAttribute("msg", new OpMsg("I", msg));
 		todoRepository.deleteById(todoData.getId());
 		return "redirect:/todo";
 	}
@@ -137,16 +157,21 @@ public class TodoListController {
 			//			todoList = todoService.doQuery(todoQuery); 
 			//			todoPage = todoDaoImpl.findByJPQL(todoQuery); //①エラーがなければ実施　②todoListのServiceのクエリを実行、　③JPQLによる検索を実行
 			todoPage = todoDaoImpl.findByCriteria(todoQuery, pageable); //Criteriaによる検索　検索条件は上記と同じ。使用する方をのこす。
-
-			//		mv.addObject("todoList", todoList); //mv.addObject("todoQuery",todoQuery);
 			session.setAttribute("todoQuery", todoQuery); //検索条件をセッションに保存しておく。Getメソッドのため、Modelを利用した保存はできない。
-
 			mv.addObject("todoPage", todoPage); //ページ情報
 			mv.addObject("todoList", todoPage.getContent());//検索結果
+			//		mv.addObject("todoList", todoList); //mv.addObject("todoQuery",todoQuery);
+
+			if (todoPage.getContent().size() == 0) { //該当がなかったらメッセージを表示する
+				String msg = messageSource.getMessage("msg.w.todo_not_found", null, locale);
+				mv.addObject("msg", new OpMsg("W", msg));
+			}
 		} else {
 			//エラーがあった場合の検索
+			String msg = messageSource.getMessage("msg.e.input_something_wrong", null, locale);
 			mv.addObject("todoPage", null);
 			mv.addObject("todoList", null);
+			mv.addObject("msg", new OpMsg("E", msg));
 		}
 		return mv;
 	}
